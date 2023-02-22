17feaaab9ad5098f60d19754813d03fb9126fea2
spathre <57168010+spathre@users.noreply.github.com>
2023-02-22 22:59:51 +0530

    initial commit

---
+++ grievance/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.contrib.auth.models import User
+from grievance_management.models import Ticket, Category
+from rest_framework import routers
+
+from grievance_management.views import UserViewSet, TicketViewSet, CategoryViewSet
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'api/users', UserViewSet)
+
+router.register(r'api/tickets', TicketViewSet)
+
+router.register(r'api/category', CategoryViewSet)
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('admin/', admin.site.urls),    
+    path(r'', include(router.urls)),
+    path(r'api/', include('rest_framework.urls', namespace='rest_framework'))
+
+]
